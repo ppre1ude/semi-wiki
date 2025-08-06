27f74897bff4d9972a56d2997a89b6698b8f743a
many-one-22 <dawon1703@gmail.com>
2025-08-07 00:16:42 +0900

    fix: 배치 수정, css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
+import './App.css';
+
 const App = () => {
   return (
     <div className="main-page-container">
-      <div className="title">
+      <div id="title">
         <h1>Semicolon;</h1>
-        <button id="button">보고서</button>
-        <button id="button">역사</button>
-        <button id="button">수정</button>
+        <button id="btn">보고서</button>
+        <button id="btn">역사</button>
+        <button id="btn">수정</button>
       </div>
 
-      <div className="main">
+      <div id="mainpage">
         <img src=""></img>
-        <p>Semicolon;<br></br>세미콜론</p>
+        <h2>Semicolon;</h2>
+        <p>세미콜론</p>
       </div>
 
-      <div></div>
+      <div id="Overview">
+        <div id="bar">개요</div>
+        <div id="bar">업적</div>
+        <div id="bar">논란 및 사건사고</div>
+      </div>
     </div>
   );
 };
