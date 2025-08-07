1026cd387c5cfdef4362cd0196fcd58f1f6a7b9d
Gwangjae Kim <rhkdwo8492@naver.com>
2025-08-07 18:40:53 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-const App = () => {
-  return <div>앗! 아직 빈칸인 세미위키 레포입니다!</div>;
-};
+import React from 'react';
+import Header from './components/Layout/Header';
+import Sidebar from './components/Layout/Sidebar';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <main style={{ padding: "20px", flex: 1 }}>
+
+        </main>
+      </div>
+    </div>
+  );
+}
 
 export default App;
