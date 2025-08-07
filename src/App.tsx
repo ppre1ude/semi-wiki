dbe40552cdb445ca1ecbe345c91c47ede8437d63
many-one-22 <dawon1703@gmail.com>
2025-08-07 19:26:14 +0900

    add: 메인 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { SearchPage } from "@/features/search/pages/SearchPage";
 import { DetailPage } from "@/features/detail/pages/DetailPage";
+import Mainpage from "@/features/mainpage/pages/Mainpage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/search" element={<SearchPage />} />
       <Route path="/article/:id" element={<DetailPage />} />
+      <Route path="/" element={<Mainpage />} />
     </Routes>
   );
 };
