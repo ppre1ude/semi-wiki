aa5b36d7d026456d22fc92db8717b47bc62d6ba7
many-one-22 <dawon1703@gmail.com>
2025-08-07 15:31:35 +0900

    design: css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import './App.css';
+import Image from './semicolon.png';
 
 const App = () => {
   return (
     <div className="main-page-container">
-      <div id="title">
+      <div className="title">
         <h1>Semicolon;</h1>
-        <button id="btn">보고서</button>
-        <button id="btn">역사</button>
-        <button id="btn">수정</button>
+        <div className="btn">
+          <button id="btn1">보고서</button>
+          <button id="btn2">역사</button>
+          <button id="btn2">수정</button>
+        </div>
       </div>
 
-      <div id="mainpage">
-        <img src=""></img>
-        <h2>Semicolon;</h2>
-        <p>세미콜론</p>
+      <hr></hr>
+
+      <div className="mainpage">
+        <div className="img">
+          <img src={Image} alt="이미지"></img>
+        </div>
+        <div className="section">
+          <h2>Semicolon;</h2>
+          <p>세미콜론</p>
+        </div>
       </div>
 
       <div id="Overview">
