2dcf9b37848341166453ec9b17eb5a99843e918c
ppre1ude <cjh5110@naver.com>
2025-08-13 19:38:48 +0900

    fix: router에 searchpage 없던 거 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,6 +4,7 @@ import AppLayout from "@/components/Layout";
 import MainPage from "@/features/main/pages/MainPage";
 import { DetailPage } from "@/features/detail/pages/DetailPage";
 import EditPage from "@/features/edit/pages/EditPage";
+import { SearchPage } from "@/features/search/pages/SearchPage";
 
 const NotFoundFallback = () => (
   <div style={{ padding: 24 }}>
@@ -16,6 +17,7 @@ export const ROUTE_PATH = {
   MAIN: "/",
   DETAIL: "/detail/:docId", // 문서 ID 파라미터 (프로젝트에 맞게 이름 조정 가능)
   EDIT: "/edit/:docId",
+  SEARCH: "/search",
   NOT_FOUND: "*",
 } as const;
 
@@ -28,6 +30,7 @@ const Router = createBrowserRouter([
       { index: true, element: <MainPage /> }, // "/"
       { path: ROUTE_PATH.DETAIL, element: <DetailPage /> }, // "/detail/:docId"
       { path: ROUTE_PATH.EDIT, element: <EditPage /> }, // "/edit/:docId"
+      { path: ROUTE_PATH.SEARCH, element: <SearchPage /> }, // "/search
       { path: ROUTE_PATH.NOT_FOUND, element: <NotFoundFallback /> },
     ],
   },
